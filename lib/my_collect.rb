d6e9c6ae325202a2c518c6a0b5718d53ff875690
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 01:43:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect (array)
 i=0
 return_array = Array.new
   while i<array.length
-  yield array[i]
-  return_array << array[i]
+  new_value = yield array[i]
+  return_array << new_value
   i+=1
 end
 return_array
